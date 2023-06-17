5d413d29308c4fc7607a0ba2d54f8ac52bff0141
David Tian <david.tian@gmail.com>
2023-06-17 02:08:37 -0700

    Cleaned up main.py.
    - Added function to print undocumented spike APIs.

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 # LEGO type:standard slot:0 autostart
+# The above line allows VS Code Lego Spike Extension to upload and run code.
+
+# region Imports 
 from math import *
 import sys
 
+from hub import battery
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from spike.operator import equal_to, greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, not_equal_to
-from hub import battery
+# endregion
 
-# ----------------  Global Initialization  -----------------
-hub = PrimeHub()
+# region Initialization
+my_hub = PrimeHub()
 distance_sensor = DistanceSensor('C')
 right_sensor = ColorSensor('F')
 left_sensor = ColorSensor('B')
 wheels = MotorPair('A', 'E')
 wheels.set_motor_rotation(20)
 matrix = LightMatrix()
+# endregion
 
+
+# region Functions
 def check_battery():
-    BATTERY = '\033[32m'
+    BATTERY_OK = '\033[32m'
     BATTERY_LOW = '\033[31m'
     ENDC = '\033[0m'
     VOLTAGE_THRESHOLD = 8000  # Threshold for battery level
@@ -28,17 +35,38 @@ def check_battery():
               " \n ----------------------------- \n >>>> please charge robot <<<< \n ----------------------------- \n"
               + ENDC)
     else:
-        print(BATTERY + "battery voltage: " + str(battery.voltage()) + ENDC)
+        print(BATTERY_OK + "battery voltage: " + str(battery.voltage()) + ENDC)
 
+
+def print_module_objects_and_attributes(module_name):
+    module = __import__(module_name)
+    print("Module: ", module_name)
+    objects = dir(module)
+
+    for obj_name in objects:
+        obj = getattr(module, obj_name)
+        attributes = dir(obj)
+        
+        print("Object: ", obj_name)
+        for attribute in attributes:
+            print("- ", attribute)
+        print()
+# endregion
+
+# region Main
 def main():
     # ----------------  Put your code logic here  -----------------
     matrix.show_image("CLOCK8")
     wheels.move(5)
     print('distance=%d' % distance_sensor.get_distance_cm())
+    
+    print_module_objects_and_attributes('hub')
+# endregion
 
-# --------------- DO NOT EDIT ANYTHING BELOW ----------------- 
+# region DO NOT EDIT ANYTHING HERE 
 check_battery()
+print("Starting... ")
 timer = Timer()
-print("starting... ")
 main()
-sys.exit("ended program. elapsed time " + str(timer.now()))
+sys.exit("Ended program. Elapsed time " + str(timer.now()))
+# endregion
